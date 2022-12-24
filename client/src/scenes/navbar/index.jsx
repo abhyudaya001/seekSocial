535d9d1deb26cac88d8f69d730060a98bdea27fe
abhyudaya001 <60521322+abhyudaya001@users.noreply.github.com>
2022-12-24 21:56:11 +0530

    Some updates

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SeekSocial
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
