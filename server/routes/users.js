ee56c89cad6e6bd7b7b7ec85dd5ac42631b26d07
abhyudaya001 <abhyudayakandwal001@gmail.com>
2022-12-24 14:23:51 +0530

    Frontend complted

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,18 @@
 import express from "express";
-import { get } from "mongoose";
-import{
-    getUser,
-    getUserFriends,
-    addRemoveFriends,
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
-const router=express.Router();
+const router = express.Router();
 
 /* READ */
-router.get("/:id",verifyToken,getUser);
-router.get("/:id/friends",verifyToken,getUserFriends);
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
-router.patch("/:id/:friendId" ,verifyToken,addRemoveFriends);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
 export default router;
